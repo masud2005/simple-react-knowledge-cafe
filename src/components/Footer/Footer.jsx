02bb2daa587511fbf3f486438aae882d3076752d
masud2005 <masud20012005@gmail.com>
2024-10-22 20:50:56 +0600

    Chages Footer Background color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-500">
+        <footer className="bg-violet-500">
             <div className="container mx-auto w-11/12 py-4">
                 <p className="text-center text-white">@Copyright Knowledge Cafe. All right reserved.</p>
             </div>
